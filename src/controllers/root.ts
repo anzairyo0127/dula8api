a4dbaf9f197d8340ff1e1f9e3876b5177b028a8d
anzai.ryo0127 <36684885+anzairyo0127@users.noreply.github.com>
2020-10-03 15:29:05 +0900

    Cognitoによる認証の追加

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -4,36 +4,31 @@ import { context, config } from "../app";
 import * as auth from "../functions/auth";
 import * as utils from "../utils";
 
-export default (middlewares: any[]) => {
-  const rooter = express.Router();
-  for (let m = 0; m < middlewares.length; m++) {
-    rooter.use(middlewares[m]);
-  };
-  rooter.get("/", (req, res, next) => {
-    const user = <string>req.user;
-    return utils.sendPayload(res, 200, 
-      { message: `Hello ${user}`}
+const rooter = express.Router();
+rooter.get("/", (req, res, next) => {
+  const user = <string>req.app.locals.username;
+  return utils.sendPayload(res, 200, 
+    { message: `Hello ${user}`}
+  );
+});
+rooter.post("/create_user", async (req, res, next) => {
+  console.log(req)
+  if (!req.body.hasOwnProperty("username") || !req.body.hasOwnProperty("password")){
+    return utils.sendPayload(res, 400,
+      { message: "Not enough data."}
     );
-  });
-  rooter.post("/create_user", async (req, res, next) => {
-    console.log(req)
-    if (!req.body.hasOwnProperty("username") || !req.body.hasOwnProperty("password")){
-      return utils.sendPayload(res, 400,
-        { message: "Not enough data."}
-      );
-    };
-    const [username, password] = [req.body["username"], req.body["password"]];
-    const [userData, isSuccess] = await auth.createNewUser(context.db, { username, password }, config.stretch);
-    if (isSuccess) {
-      return utils.sendPayload(res, 200, {
-        message: `Create success Username:${userData.username}.`,
-        userData: userData,
-      });
-    } else {
-      return utils.sendPayload(res, 400, {
-        message: `Create failed.`
-      });
-    };
-  });  
-  return rooter;
-};
+  };
+  const [username, password] = [req.body["username"], req.body["password"]];
+  const [userData, isSuccess] = await auth.createNewUser(context.db, { username, password }, config.stretch);
+  if (isSuccess) {
+    return utils.sendPayload(res, 200, {
+      message: `Create success Username:${userData.username}.`,
+      userData: userData,
+    });
+  } else {
+    return utils.sendPayload(res, 400, {
+      message: `Create failed.`
+    });
+  };
+});  
+export default rooter;
